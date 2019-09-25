815cf6a055de73f21afef89469c28cb8aa3ca3cf
Luan Santos <501526@soa.ad.pwcinternal.com>
2019-09-25 10:30:58 -0300

    exercicio de orientação a objeto

---
+++ exercicios/ex01.py
@@ -0,0 +1,7 @@
+"""=============================================================================
+Criar Classe Pessoa que receba os atributos(idade, altura e cor_dos_olhos) e
+tenha petodos para retornar(return) estas informações
+============================================================================="""
+
+
+# Faça a classe aqui...
