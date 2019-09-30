2f8a9369e02e8ee9fad11fc2e8bc9f532a96f766
Aline Soares <alineb.soarees22@gmail.com>
2019-09-30 17:03:30 -0300

    primeiro exercicio

--- exercicios/ex01.py
+++ exercicios/ex01.py
@@ -1,7 +1,21 @@
 """=============================================================================
 Criar Classe Pessoa que receba os atributos(idade, altura e cor_dos_olhos) e
-tenha petodos para retornar(return) estas informações
+tenha metodos para retornar(return) estas informações
 ============================================================================="""
 
 
 # Faça a classe aqui...
+class Pessoa():
+    def __init__(self, idade, altura, cor_dos_olhos):
+        self.idade = idade
+        self.altura = altura
+        self.cor_dos_olhos = cor_dos_olhos
+
+    def retorna_idade(self):
+        return self.idade
+
+    def retorna_altura(self):
+        return self.altura
+
+    def retorna_olhos(self):
+        return self.cor_dos_olhos       
